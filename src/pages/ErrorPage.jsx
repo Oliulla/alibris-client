76c3dc1bb7e34f96cbb567fe43d7c4ff93c2fdb4
Oliulla <razasm29@gmail.com>
2022-11-27 07:19:32 +0600

    added errorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div className="flex justify-center gap-4 w-full h-screen items-center">
+      {error && (
+        <>
         <div>
-            <h2>Error page is comming...</h2>
+            <img className="w-80" src="https://i.ibb.co/mBvQGMg/errorImg.jpg" alt="" />
         </div>
-    );
+        <div className="flex flex-col items-center">
+          {
+            <>
+              <h2 className="text-4xl font-bold text-red-600">Opps!!!</h2>
+              <p className="text-3xl font-bold">{error?.status}</p>
+              <h2 className="text-2xl font-bold">{error.statusText}</h2>
+            </>
+          }
+        </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ErrorPage;
