8c1e981e7d3f1f100d6a175d47aefbca8113e1cf
oliullah <razasm29@gmail.com>
2022-12-19 21:40:40 +0600

    fix product delete bug and design better for ux

---
+++ src/components/OpenModal/PermissionModal.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { toast } from "react-hot-toast";
+
+const PermissionModal = ({ productId, refetch }) => {
+
+  // delete specific product
+  const handleProductDelete = (deletedProductId) => {
+
+    fetch(`http://localhost:5000/myproducts/${deletedProductId}`, {
+      method: 'Delete',
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      toast.success('Successfully deleted!');
+      refetch();
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  };
+
+
+  return (
+    <div>
+      {/* Put this part before </body> tag */}
+      <input
+        type="checkbox"
+        id="product-delet-modal"
+        className="modal-toggle"
+      />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <div className="card-actions items-center flex-col gap-4 mr-10">
+            <h3 className="text-lg font-bold">Are you sure to delete?</h3>
+            <div>
+              <label
+                htmlFor="product-delet-modal"
+                className="btn btn-sm p-2 m-2 btn-warning"
+                onClick={() => handleProductDelete(productId)}
+              >
+                Delete
+              </label>
+              <label
+                htmlFor="product-delet-modal"
+                className="btn btn-sm p-2 m-2"
+              >
+                Cancel
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PermissionModal;
--- src/pages/Dashboard/MyProducts.jsx
+++ src/pages/Dashboard/MyProducts.jsx
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import Loading from "../../components/Loading";
 import { AuthContext } from "../../contexts/AuthProvider";
 import toast from "react-hot-toast";
+import PermissionModal from "../../components/OpenModal/PermissionModal";
 // import useRefetch from "../../hooks/useRefetch";
 
 const MyProducts = () => {
@@ -23,11 +24,12 @@ const MyProducts = () => {
       const data = await axios.get(
         `http://localhost:5000/myproducts?email=${user?.email}`
       );
-      var productsArr = data.data.data;
-      var products = Array.prototype.concat.apply([], productsArr);
-      // console.log(products);
-      // setIsProductLoading(!isProductLoading)
-      return products;
+      // var productsArr = data.data.data;
+      // var products = Array.prototype.concat.apply([], productsArr);
+      // // console.log(products);
+      // // setIsProductLoading(!isProductLoading)
+      // console.log(data.data.data);
+      return data?.data?.data;
     },
   });
 
@@ -57,27 +59,15 @@ const MyProducts = () => {
       });
   };
 
-  // need aggregate to delete this product
-  const handleProductDelete = (deletedProduct) => {
-    // console.log(myProducts, deletedProduct);
-    // console.log(deletedProduct);
-    // fetch(`http://localhost:5000/myproducts/${_id}`, {
-    //   method: 'PUT',
-    //   body: JSON.stringify(deletedProduct)
-    // })
-    // .then(res => res.json())
-    // .then(data => {
-    //   console.log(data)
-    // })
-  };
-
   return (
     <>
       {!myProducts.length ? (
-        <p className="text-3xl mt-4">You have not added any product</p>
+        <p className="text-3xl mt-4 text-center py-12">
+          You have not added any product
+        </p>
       ) : (
         <div className="overflow-x-auto px-8 my-10">
-          <h2 className="text-center text-3xl mb-4 border-b-2 border-secondary">
+          <h2 className="text-center text-2xl font-semibold mb-4 border-b-2 border-secondary">
             My Products
           </h2>
           <table className="table w-full">
@@ -85,34 +75,43 @@ const MyProducts = () => {
               <tr>
                 <th>Name</th>
                 <th>Price</th>
-                <th>Available/Sold</th>
+                <th>Available</th>
+                <th>Addvertise</th>
                 <th>Actions</th>
               </tr>
             </thead>
             <tbody>
-              {myProducts.map((singleProduct, idx) => {
+              {myProducts.map((singleProduct) => {
                 return (
-                  <tr key={idx}>
+                  <tr key={singleProduct._id}>
                     <td className="font-bold">{singleProduct.bookName}</td>
                     <td>{singleProduct.resalePrice}tk</td>
                     <td>
-                      <button className="text-green-700">
+                      <span className="font-semibold text-green-700">
                         {singleProduct.isAvailable ? "Unsold" : "Sold"}
-                      </button>
-                      <button
-                        className="ml-4 text-blue-800"
-                        onClick={() => handleAdvertise(singleProduct)}
-                      >
-                        {singleProduct.isAvailable ? "Advertise" : ""}
-                      </button>
+                      </span>
                     </td>
                     <td>
                       <button
-                        onClick={() => handleProductDelete(singleProduct)}
+                      
+                        className={`btn btn-sm btn-primary text-white font-extrabold ${singleProduct.isAvailable ? undefined : "btn-disabled"}`}
+                        onClick={() => handleAdvertise(singleProduct)}
+                      >
+                        Advertise
+                      </button>
+                    </td>
+                    <td>
+                      {/* <label htmlFor="my-modal-6" className="btn">open modal</label> */}
+                      <label
+                        htmlFor="product-delet-modal"
                         className="btn btn-sm bg-red-500"
                       >
                         Delete
-                      </button>
+                      </label>
+                      <PermissionModal
+                        productId={singleProduct._id}
+                        refetch={refetch}
+                      />
                     </td>
                   </tr>
                 );
